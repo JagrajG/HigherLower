e5230cc4dd27ebdbea98a258f6a78edd72908f47
Jagraj Gill <jagrajgill@d142-058-084-120.wireless.sfu.ca>
2024-05-27 12:50:23 -0700

    added score counter

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let secretNumber = Math.trunc(Math.random() * 10) + 1;
+let score = 20;
 console.log(secretNumber);
 document.querySelector(".btn").addEventListener("click", function () {
   let guess = Number(document.querySelector(".guess").value);
@@ -10,17 +11,23 @@ document.querySelector(".btn").addEventListener("click", function () {
   } else if (!guess) {
     document.querySelector(".set").textContent = "Please enter a number";
   } else if (guess > secretNumber) {
-    document.querySelector(".set").textContent = "To big";
+    score--;
+    document.querySelector(".set").textContent = "Too big";
+    document.querySelector(".attempts").textContent = "Score: " + score;
   } else if (guess < secretNumber) {
-    document.querySelector(".set").textContent = "To small";
+    score--;
+    document.querySelector(".set").textContent = "Too small";
+    document.querySelector(".attempts").textContent = "Score: " + score;
   }
 });
 
 document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
   secretNumber = Math.trunc(Math.random() * 10) + 1;
   console.log(secretNumber);
   document.querySelector("body").style.backgroundColor = "#FFFFFF";
   document.querySelector(".set").textContent = "Start guessing...";
   document.querySelector(".secretNum").textContent = "???";
   document.querySelector(".guess").value = "";
+  document.querySelector(".attempts").textContent = "Score: " + score;
 });
