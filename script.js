bd89ade86f5371ef93eb182b3ecd160f451a273b
Jagraj Gill <jagrajgill@Jagrajs-MBP.lan>
2024-05-27 21:38:35 -0700

    added simple fixes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let secretNumber = Math.trunc(Math.random() * 10) + 1;
 let score = 20;
+let highScore = 0;
 console.log(secretNumber);
 document.querySelector(".btn").addEventListener("click", function () {
   let guess = Number(document.querySelector(".guess").value);
@@ -8,16 +9,36 @@ document.querySelector(".btn").addEventListener("click", function () {
     document.querySelector(".set").textContent = "Correct";
     document.querySelector(".secretNum").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent =
+        "Highscore: " + highScore;
+    }
   } else if (!guess) {
     document.querySelector(".set").textContent = "Please enter a number";
   } else if (guess > secretNumber) {
-    score--;
-    document.querySelector(".set").textContent = "Too big";
-    document.querySelector(".attempts").textContent = "Score: " + score;
+    if (score > 1) {
+      score--;
+      document.querySelector(".set").textContent = "Too big";
+      document.querySelector(".attempts").textContent = "Score: " + score;
+    } else {
+      document.querySelector(".set").textContent = "You Lost!";
+      document.querySelector(".attempts").textContent = 0;
+      document.querySelector("body").style.backgroundColor = "#E40000";
+      document.querySelector(".secretNum").textContent = secretNumber;
+    }
   } else if (guess < secretNumber) {
-    score--;
-    document.querySelector(".set").textContent = "Too small";
-    document.querySelector(".attempts").textContent = "Score: " + score;
+    if (score > 1) {
+      score--;
+      document.querySelector(".set").textContent = "Too small";
+      document.querySelector(".attempts").textContent = "Score: " + score;
+    } else {
+      document.querySelector(".set").textContent = "You Lost!";
+      document.querySelector(".attempts").textContent = 0;
+      document.querySelector("body").style.backgroundColor = "#E40000";
+      document.querySelector(".secretNum").textContent = secretNumber;
+    }
   }
 });
 
